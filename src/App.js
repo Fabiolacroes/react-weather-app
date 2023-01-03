b035e3cb2f01922e17699afaf14a7d18f4f6f800
Fabiolacroes <fabiolacroes@gmail.com>
2023-01-03 15:24:24 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Fabiola Croes and is{" "}
-        <a href="https://github.com/Fabiolacroes/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Fabiola Croes and is{" "}
+          <a href="https://github.com/Fabiolacroes/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
